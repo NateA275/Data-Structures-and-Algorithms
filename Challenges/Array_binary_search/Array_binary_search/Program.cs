f9fc8c4293c1c94cd247cfd3d3ab1a4ca27190cf
NateA275 <nateaurich@gmail.com>
2018-06-06 11:37:32 -0700

    Git issues

--- Challenges/Array_binary_search/Array_binary_search/Program.cs
+++ Challenges/Array_binary_search/Array_binary_search/Program.cs
@@ -13,25 +13,31 @@ namespace Array_binary_search
             Console.ReadLine();
         }
 
+
+        /* BinarySearch method - Utilizes binary search to find the index of a value within a sorted array
+         * @param integer array arr - Sorted array of integers to be searched
+         * @param integer key - Value to be found within sorted array
+         * @return intger - Index of selected value within array or -1 if value DNE
+         */ 
         static int BinarySearch(int[] arr, int key)
         {
-            int left = 0;
-            int right = arr.Length - 1;
+            int left = 0; //Delcare left post index
+            int right = arr.Length - 1; //Declare right post index
 
             while (left <= right)
             {
-                int middle = (left + right) / 2;
-                if (arr[middle] == key)
-                    return middle;
+                int middle = (left + right) / 2; //Declare middle value index
+                if (arr[middle] == key) //Evaluate middle value
+                    return middle; //Return Index
 
                 if (arr[middle] > key)
-                    right = middle - 1;
+                    right = middle - 1; //Reassign right post
 
                 if (arr[middle] < key)
-                    left = middle + 1;
+                    left = middle + 1; //Reassign left post
             }
 
-            return -1;
+            return -1; //Return -1 if not found
 
         }
     }
