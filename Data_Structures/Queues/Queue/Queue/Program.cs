836e40e8bc4f143caf250aba4520acbd58d7178f
NateA275 <nateaurich@gmail.com>
2018-06-19 14:21:26 -0700

    Fixed Directory organization

---
+++ Data_Structures/Queues/Queue/Queue/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Queue.Classes;
+
+namespace Queue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
