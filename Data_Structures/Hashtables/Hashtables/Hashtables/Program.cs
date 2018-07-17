d0d822e6651493c97f20ba9011f9b6fb6d67f876
NateA275 <nateaurich@gmail.com>
2018-07-16 21:58:26 -0700

    Added main function

--- Data_Structures/Hashtables/Hashtables/Hashtables/Program.cs
+++ Data_Structures/Hashtables/Hashtables/Hashtables/Program.cs
@@ -15,8 +15,19 @@ namespace Hashtables
                 table.Add("doe", 25);
                 table.Add("Cat", 17);
 
-                Console.WriteLine(table.Find("cat"));
-                Console.WriteLine(table.Find("doe"));
+
+                Console.WriteLine($"'cat' exists in hashtable: {table.Contains("cat")}");
+                Console.WriteLine("Expected value of 'cat': 50");
+                Console.WriteLine($"Found: {table.Find("cat")}");
+
+                Console.WriteLine($"'doe' exists in hashtable: {table.Contains("doe")}");
+                Console.WriteLine("Expected value of 'doe': 25");
+                Console.WriteLine($"Found: {table.Find("doe")}");
+
+                Console.WriteLine($"'Cat' exists in hashtable: {table.Contains("Cat")}");
+                Console.WriteLine("Expected value of 'Cat': 17");
+                Console.WriteLine($"Found: {table.Find("Cat")}");
+
             }
             catch (Exception ex)
             {
