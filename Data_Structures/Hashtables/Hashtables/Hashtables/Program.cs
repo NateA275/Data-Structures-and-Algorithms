fdd14de8bd7b54372eeec24ba8fdac44a938a59d
NateA275 <nateaurich@gmail.com>
2018-07-16 21:47:15 -0700

    completed hashtable implementation

---
+++ Data_Structures/Hashtables/Hashtables/Hashtables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Hashtables.Classes;
+
+namespace Hashtables
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable table = new Hashtable();
+
+            try
+            {
+                table.Add("cat", 50);
+                table.Add("doe", 25);
+                table.Add("Cat", 17);
+
+                Console.WriteLine(table.Find("cat"));
+                Console.WriteLine(table.Find("doe"));
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
